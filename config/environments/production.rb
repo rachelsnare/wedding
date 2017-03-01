f10c5f9fa20157e748fa4b8bda676d4d96594964
ignaciosm <ignaciosm@gmail.com>
2017-03-01 15:42:52 -0500

    add aws s3 for use with paperclip in heroku 4 try

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,14 +84,14 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # config/environments/production.rb
+# config/environments/production.rb
 config.paperclip_defaults = {
   storage: :s3,
   s3_credentials: {
-    bucket: ENV.fetch('rachelandignacio-assets'),
-    access_key_id: ENV.fetch('AKIAJ6KP43NJZ5KA6JRA'),
-    secret_access_key: ENV.fetch('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'),
-    s3_region: ENV.fetch('us-west-1'),
+    bucket: ENV.fetch('S3_BUCKET_NAME'),
+    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    s3_region: ENV.fetch('AWS_REGION'),
   }
 }
 end
